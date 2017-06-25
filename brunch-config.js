8c62d679394fa148719034ae508d3d1c96d4861f
leezye <aetwaroo@accudocsolutions.com>
2017-06-24 21:31:49 -0400

    clean

--- brunch-config.js
+++ brunch-config.js
@@ -1,13 +1,17 @@
 // See http://brunch.io for documentation.
 exports.files = {
-  javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
+  javascripts: {
+    joinTo: 'app.js'
+  },
+  stylesheets: {
+    joinTo: 'app.css'
+  }
 };
 
 exports.plugins = {
-    sass: {
-        options: {
-            includePaths: ['node_modules/bootstrap/scss']
-        }
+  sass: {
+    options: {
+      includePaths: ['node_modules/bootstrap/scss']
     }
+  }
 }
