05e45bfacbc323c325690b60487768514532abe2
leezye <aetwaroo@accudocsolutions.com>
2017-06-28 16:24:18 -0400

    update

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var Turbolinks = require("turbolinks")
-  Turbolinks.start()
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+/*eslint no-console: ["error", { allow: ["log","warn", "error"] }] */
+document.addEventListener("DOMContentLoaded", function() {
+    var Turbolinks = require("turbolinks");
+    Turbolinks.start();
 });
