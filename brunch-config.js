65d1d5c79c42696d335f5a9078c933e53f47a379
leezye <aetwaroo@accudocsolutions.com>
2017-07-07 10:59:07 -0400

    update

--- brunch-config.js
+++ brunch-config.js
@@ -13,14 +13,5 @@ exports.plugins = {
         options: {
             includePaths: ["node_modules/bootstrap/scss"]
         }
-    },
-    purifycss: {
-        options: {
-            target: {
-                src: ["public/index.html"],
-                css: ["public/*.css"],
-                dest: "public/purestyles.css"
-            }
-        }
     }
 };
