b90d5863f30d1b21dba98c8dfe0dfd4b6b8f727e
leezye <aetwaroo@accudocsolutions.com>
2017-07-07 22:31:03 -0400

    fix eslint error (console.log)

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -1,4 +1,5 @@
 'use strict';
 
+/*eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 console.log('\'Allo \'Allo!');
 //# sourceMappingURL=main.js.map
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,2 @@
+/*eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 console.log('\'Allo \'Allo!');
